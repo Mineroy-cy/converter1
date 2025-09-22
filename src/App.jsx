310a653ee0e0dded4e13d4c798a7db1d7c73750f
Mineroy-cy <royvictorthuku@gmail.com>
2025-09-23 00:43:54 +0300

    3nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ConversionPage from "./pages/ConversionPage";
 import ArithmeticPage from "./pages/ArithmeticPage";
 
 export default function App() {
   return (
-    <Router basename="/converter1">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/conversion" element={<ConversionPage />} />
-        <Route path="/arithmetic" element={<ArithmeticPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/conversion" element={<ConversionPage />} />
+      <Route path="/arithmetic" element={<ArithmeticPage />} />
+    </Routes>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App/>
+    <BrowserRouter basename="/converter1">
+      <App />
+    </BrowserRouter>
   </StrictMode>
-)
-
+);
