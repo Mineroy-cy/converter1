248c927a68ab0ae0a484e7f8fa81547a249d718e
Mineroy-cy <royvictorthuku@gmail.com>
2025-09-23 01:07:05 +0300

    mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom"; 
 import Home from "./pages/Home";
 import ConversionPage from "./pages/ConversionPage";
 import ArithmeticPage from "./pages/ArithmeticPage";
