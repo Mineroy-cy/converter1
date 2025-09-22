ed93c227346afad559a5ed365f1ebd7036967566
Mineroy-cy <royvictorthuku@gmail.com>
2025-09-23 01:04:09 +0300

    mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import ConversionPage from "./pages/ConversionPage";
 import ArithmeticPage from "./pages/ArithmeticPage";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/conversion" element={<ConversionPage />} />
-      <Route path="/arithmetic" element={<ArithmeticPage />} />
-
-      {/* fallback route: anything unknown goes to Home */}
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/conversion" element={<ConversionPage />} />
+        <Route path="/arithmetic" element={<ArithmeticPage />} />
+        
+        {/* fallback route: redirect unknown URLs to Home */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   );
 }
