e45be8f80dc9fbda51eb07c6bd533914b8957624
Mineroy-cy <royvictorthuku@gmail.com>
2025-09-29 10:58:33 +0300

    pwa2

--- vite.config.js
+++ vite.config.js
@@ -5,29 +5,9 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
+     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
-      manifest: {
-        name: "Number System Toolkit",
-        short_name: "NumToolkit",
-        start_url: ".",
-        display: "standalone",
-        background_color: "#ffffff",
-        theme_color: "#4CAF50",
-        icons: [
-          {
-            src: "/icons/icon-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "/icons/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png"
-          }
-        ]
-      }
+      manifest: true, // uses your manifest.json
     })
   ],
    base: '/converter1/' // 👈 must match your repo name for GitHub Pages
