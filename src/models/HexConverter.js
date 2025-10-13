9b2837156b2042656a002abe1591c7ff652c4bcd
Pilatu-cell <tma854385@gmail.com>
2025-10-13 12:06:24 +0300

    Made some few changes on the hexconverter module

--- src/models/HexConverter.js
+++ src/models/HexConverter.js
@@ -1,10 +1,23 @@
 import Converter from "./Converter";
 
+/**
+ * Converter class for hexadecimal number operations
+ * @extends Converter
+ */
 export default class HexConverter extends Converter {
+  /**
+   * Converts the hexadecimal value to decimal
+   * @returns {number} The decimal representation of the hexadecimal value
+   */
   toDecimal() {
     return parseInt(this.value, 16);
   }
 
+  /**
+   * Converts a decimal number to its hexadecimal representation
+   * @param {number} decimal - The decimal number to convert
+   * @returns {string} The uppercase hexadecimal representation of the decimal number
+   */
   static fromDecimal(decimal) {
     return decimal.toString(16).toUpperCase();
   }
